23df1d956e94150bc0fbe251488c72ba44422088
xgroleau🐢 <xavgroleau@gmail.com>
2023-12-03 10:43:50 -0500

    feat: day1 step 1

--- lib/problems/problem_2023_01.ml
+++ lib/problems/problem_2023_01.ml
@@ -3,14 +3,21 @@ open Core;;
 let year = 2023
 let day = 1
 
+
 module Part_1 = struct
 
     let run (input : string) : (string, string) result =
+       let lines = String.split_lines  input in
 
-       let new_line = String.split_on_char '\n' input in
-      (* strin *)
-      (* let str = input |> new_line |> String.to_seq in *)
-      Ok input
+
+       let total = List.fold lines ~init:0 ~f:(fun acc line ->
+           let chars = String.to_list  line in
+           let numbers = List.filter chars ~f:Char.is_digit in
+           let first = List.hd_exn numbers in
+           let last = List.last_exn numbers in
+           let number = Fmt.str "%c%c" first last |> Int.of_string in
+         acc + number) |> Fmt.str "%d" in
+      Ok total
 end
 
 module Part_2 = struct
