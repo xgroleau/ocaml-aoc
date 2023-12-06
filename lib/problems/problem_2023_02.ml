096b79ba93f364f2134e61be9ba37abdc8cec553
Xavier Groleau <xavgroleau@gmail.com>
2023-12-05 20:02:24 -0500

    feat: day2 problem2 done

--- lib/problems/problem_2023_02.ml
+++ lib/problems/problem_2023_02.ml
@@ -58,7 +58,54 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
+    exception Error of string
+
+    let sort_colors_exn (red, green ,blue) str num =
+      match str with
+      | "red" -> (num :: red, green, blue)
+      | "green" -> (red, num :: green, blue)
+      | "blue" -> (red, green, num :: blue)
+      | _ -> raise (Error "Failed, invalid color type")
+    ;;
+
+    let handle_draw num_list str =
+      let game_data = String.split str ~on:' '
+        |> List.filter ~f:(fun s -> not (String.is_empty s)) in
+      let num = List.hd_exn game_data |> int_of_string in
+      let color = List.last_exn game_data in
+      sort_colors_exn num_list color num
+    ;;
+
+
+    let parse_draw num_list str =
+      let draw = String.split str ~on:',' in
+      List.fold draw ~init:num_list ~f:(fun accum value ->
+          handle_draw accum value
+        )
+    ;;
+
+
+    let parse_draws str =
+      let draws = String.split str ~on:';' in
+      let (red, green, blue) = List.fold draws ~init:([], [], [])  ~f:(fun accum value ->
+          parse_draw accum value
+        ) in
+      let max_in_list lst = match List.max_elt lst ~compare:Int.compare with
+      | Some value -> value
+      | None -> raise (Error "Empty list") in
+      let red_max = max_in_list red in
+      let green_max = max_in_list green in
+      let blue_max = max_in_list blue in
+      red_max * green_max * blue_max
+    ;;
+
     let run (input : string) : (string, string) result =
-      Ok input
+       let lines = String.split_lines input in
+       let total = List.fold ~init:0 lines ~f:(fun accum value ->
+           let (_, draws) = String.lsplit2_exn value ~on:':' in
+           let power = parse_draws draws in
+           accum + power
+         ) |> string_of_int in
+      Ok total
     ;;
 end
