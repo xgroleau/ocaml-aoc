82a587ee373bc21a9322b2443f9e1b9bb6202244
Xavier Groleau <xavgroleau@gmail.com>
2023-12-05 20:20:35 -0500

    wip: problem 3

---
+++ lib/problems/problem_2023_03.ml
@@ -0,0 +1,17 @@
+open Core;;
+
+let year = 2023
+let day = 3
+
+
+module Part_1 = struct
+    let run (input : string) : (string, string) result =
+      Ok input
+    ;;
+end
+
+module Part_2 = struct
+    let run (input : string) : (string, string) result =
+      Ok input
+    ;;
+end
