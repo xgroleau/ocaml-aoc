cbf8d631b5c91c32cfdc57363023875bfd9e72bf
Xavier Groleau <xavgroleau@gmail.com>
2023-12-07 18:56:02 -0500

    wip problem 3

--- lib/problems/problem_2023_02.ml
+++ lib/problems/problem_2023_02.ml
@@ -1,111 +1,98 @@
-open Core;;
+open Core
 
 let year = 2023
 let day = 2
 
-
 module Part_1 = struct
+  let handle_draw str =
+    let game_data = String.split str ~on:' ' |> List.filter ~f:(fun s -> not (String.is_empty s)) in
+    let num = List.hd_exn game_data |> int_of_string in
+    let color = List.last_exn game_data in
+    match color with
+    | "red" -> num <= 12
+    | "green" -> num <= 13
+    | "blue" -> num <= 14
+    | _ -> false
+  ;;
 
-    let handle_draw str =
-      let game_data = String.split str ~on:' '
-        |> List.filter ~f:(fun s -> not (String.is_empty s)) in
-      let num = List.hd_exn game_data |> int_of_string in
-      let color = List.last_exn game_data in
-      match color with
-      | "red" -> num <= 12
-      | "green" -> num <= 13
-      | "blue" -> num <= 14
-      | _ -> false
-    ;;
+  let parse_draw str =
+    let draw = String.split str ~on:',' in
+    List.for_all draw ~f:(fun value -> handle_draw value)
+  ;;
 
+  let parse_draws str =
+    let draws = String.split str ~on:';' in
+    List.for_all draws ~f:(fun value -> parse_draw value)
+  ;;
 
-    let parse_draw str =
-      let draw = String.split str ~on:',' in
-      List.for_all draw ~f:(fun (value) ->
-          handle_draw value
-        )
-    ;;
+  let get_game_id str =
+    let game, rest = String.lsplit2_exn str ~on:':' in
+    let game_id = String.sub ~pos:5 ~len:(String.length game - 5) game in
+    let game_id = int_of_string game_id in
+    game_id, rest
+  ;;
 
-
-    let parse_draws str =
-      let draws = String.split str ~on:';' in
-      List.for_all draws ~f:(fun (value) ->
-          parse_draw value
-        )
-    ;;
-
-    let get_game_id str =
-      let (game, rest) = String.lsplit2_exn str ~on:':' in
-      let game_id = String.sub ~pos:5 ~len:(String.length game - 5) game in
-      let game_id = int_of_string game_id in
-      (game_id, rest)
-    ;;
-
-
-
-    let run (input : string) : (string, string) result =
-       let lines = String.split_lines input in
-       let total = List.filter_map lines ~f:(fun (value) ->
-           let (id, rest) = get_game_id value in
-           match parse_draws rest with
-           | true -> Some id
-           | false -> None
-         ) |> List.fold_left ~init:0 ~f:(+)
-           |> string_of_int in
-
-      Ok total
-    ;;
+  let run (input : string) : (string, string) result =
+    let lines = String.split_lines input in
+    let total =
+      List.filter_map lines ~f:(fun value ->
+        let id, rest = get_game_id value in
+        match parse_draws rest with
+        | true -> Some id
+        | false -> None)
+      |> List.fold_left ~init:0 ~f:( + )
+      |> string_of_int
+    in
+    Ok total
+  ;;
 end
 
 module Part_2 = struct
-    exception Error of string
+  exception Error of string
 
-    let sort_colors_exn (red, green ,blue) str num =
-      match str with
-      | "red" -> (num :: red, green, blue)
-      | "green" -> (red, num :: green, blue)
-      | "blue" -> (red, green, num :: blue)
-      | _ -> raise (Error "Failed, invalid color type")
-    ;;
+  let sort_colors_exn (red, green, blue) str num =
+    match str with
+    | "red" -> num :: red, green, blue
+    | "green" -> red, num :: green, blue
+    | "blue" -> red, green, num :: blue
+    | _ -> raise (Error "Failed, invalid color type")
+  ;;
 
-    let handle_draw num_list str =
-      let game_data = String.split str ~on:' '
-        |> List.filter ~f:(fun s -> not (String.is_empty s)) in
-      let num = List.hd_exn game_data |> int_of_string in
-      let color = List.last_exn game_data in
-      sort_colors_exn num_list color num
-    ;;
+  let handle_draw num_list str =
+    let game_data = String.split str ~on:' ' |> List.filter ~f:(fun s -> not (String.is_empty s)) in
+    let num = List.hd_exn game_data |> int_of_string in
+    let color = List.last_exn game_data in
+    sort_colors_exn num_list color num
+  ;;
 
+  let parse_draw num_list str =
+    let draw = String.split str ~on:',' in
+    List.fold draw ~init:num_list ~f:(fun accum value -> handle_draw accum value)
+  ;;
 
-    let parse_draw num_list str =
-      let draw = String.split str ~on:',' in
-      List.fold draw ~init:num_list ~f:(fun accum value ->
-          handle_draw accum value
-        )
-    ;;
-
-
-    let parse_draws str =
-      let draws = String.split str ~on:';' in
-      let (red, green, blue) = List.fold draws ~init:([], [], [])  ~f:(fun accum value ->
-          parse_draw accum value
-        ) in
-      let max_in_list lst = match List.max_elt lst ~compare:Int.compare with
+  let parse_draws str =
+    let draws = String.split str ~on:';' in
+    let red, green, blue = List.fold draws ~init:([], [], []) ~f:(fun accum value -> parse_draw accum value) in
+    let max_in_list lst =
+      match List.max_elt lst ~compare:Int.compare with
       | Some value -> value
-      | None -> raise (Error "Empty list") in
-      let red_max = max_in_list red in
-      let green_max = max_in_list green in
-      let blue_max = max_in_list blue in
-      red_max * green_max * blue_max
-    ;;
+      | None -> raise (Error "Empty list")
+    in
+    let red_max = max_in_list red in
+    let green_max = max_in_list green in
+    let blue_max = max_in_list blue in
+    red_max * green_max * blue_max
+  ;;
 
-    let run (input : string) : (string, string) result =
-       let lines = String.split_lines input in
-       let total = List.fold ~init:0 lines ~f:(fun accum value ->
-           let (_, draws) = String.lsplit2_exn value ~on:':' in
-           let power = parse_draws draws in
-           accum + power
-         ) |> string_of_int in
-      Ok total
-    ;;
+  let run (input : string) : (string, string) result =
+    let lines = String.split_lines input in
+    let total =
+      List.fold ~init:0 lines ~f:(fun accum value ->
+        let _, draws = String.lsplit2_exn value ~on:':' in
+        let power = parse_draws draws in
+        accum + power)
+      |> string_of_int
+    in
+    Ok total
+  ;;
 end
--- lib/problems/problem_2023_03.ml
+++ lib/problems/problem_2023_03.ml
@@ -1,17 +1,84 @@
-open Core;;
+open Core
 
 let year = 2023
 let day = 3
+let neighborhood : (int * int) list = [ -1, -1; -1, 0; -1, 1; 0, -1; 0, 1; 1, -1; 1, 0; 1, 1 ]
+let is_dot (a : char) : bool = Char.( = ) a '.'
+let is_number (a : char) : bool = Char.between a ~low:'0' ~high:'9'
+let is_symbol (a : char) : bool = a |> is_number |> not && a |> is_number |> not
+
+(* Step 2: Group contiguous characters into strings along with their starting xitions *)
+let rec generate_number_list acc str start_x x y = function
+  | [] ->
+    (* Add the last string to the accumulator, if any *)
+    if String.length str > 0 then (start_x, y, str) :: acc else acc
+  | (nx, ny, c) :: tail ->
+    if nx = x + 1 && y = ny
+    then
+      (* Contiguous character: append it to the current string *)
+      generate_number_list acc (str ^ Char.escaped c) start_x nx ny tail
+    else (
+      (* New string starts: add the current string to the accumulator, if any, and start a new string *)
+      let acc = if String.length str > 0 then (start_x, y, str) :: acc else acc in
+      generate_number_list acc (Char.escaped c) nx nx ny tail)
+;;
+(* let print_tuple (a, b) = *)
+(*   Printf.printf "(%d, %d)" a b *)
+
+(* let print_list_of_tuples lst = *)
+(*   List.iter  lst ~f:(fun tuple -> print_tuple tuple; print_string "; "); *)
+(*   Printf.printf "\n"; *)
+(*   () *)
 
 
 module Part_1 = struct
-    let run (input : string) : (string, string) result =
-      Ok input
-    ;;
+  let run (input : string) : (string, string) result =
+    (* Printf.printf "%s" input; *)
+    let lines = String.split_lines input in
+    let values =
+      List.mapi lines ~f:(fun y line ->
+        let chars = String.to_list line in
+        List.filter_mapi chars ~f:(fun x c -> if is_dot c then None else Some (x, y, c)))
+      |> List.concat
+    in
+    (* Positions that touches the symbol *)
+    let valid_pos =
+      List.filter_map values ~f:(fun (x, y, c) ->
+        match is_symbol c with
+        | true -> Some (x, y)
+        | false -> None)
+      |> List.map ~f:(fun (x, y) -> List.map neighborhood ~f:(fun (nx, ny) -> nx + x, ny + y))
+      |> List.concat
+    in
+    (* print_list_of_tuples valid_pos; *)
+    (* The numbers *)
+    let nums =
+      List.filter values ~f:(fun (_x, _y, c) -> is_number c)
+      |> List.sort ~compare:(fun (x1, y1, _) (x2, y2, _) -> if y1 <> y2 then Int.compare y1 y2 else Int.compare x1 x2)
+    in
+    let sx, sy, sc = List.hd_exn nums in
+    let tail = List.tl_exn nums in
+    let numbers_list = generate_number_list [] (Char.escaped sc) sx sx sy tail in
+    (* Filter out the invalid number string *)
+    let value =
+      List.filter_map numbers_list ~f:(fun (x, y, s) ->
+        let valid = List.exists valid_pos ~f:(fun (vx, vy) ->
+              if x <= vx && vx <= x + String.length s && vy = y then
+                Printf.printf "Value pos %d %d: %d Res %B %B %B\n" x (x + String.length s)  vx (x <= vx) (vx <= (x + String.length s)) (vy = y);
+              x <= vx && vx <= x + String.length s && vy = y
+            ) in
+        match valid with
+        | true ->
+          (* Printf.printf "Valid pos %d %d, %s\n" x y s; *)
+          Some (int_of_string s)
+        | false -> None)
+      |> List.fold_left ~init:0 ~f:( + )
+      |> string_of_int
+    in
+    Ok value
+  ;;
 end
 
 module Part_2 = struct
-    let run (input : string) : (string, string) result =
-      Ok input
-    ;;
+  let run (input : string) : (string, string) result = Ok input
 end
